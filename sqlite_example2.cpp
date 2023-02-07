78a52846842b8fe144777b309a739489ebd2d834
seraptas <71277876+srptas@users.noreply.github.com>
2023-02-08 01:07:26 +0300

    Add files via upload

--- sqlite_example2.cpp
+++ sqlite_example2.cpp
@@ -32,9 +32,11 @@ void tokenize(std::string const& str, const char* delim,
 
 using namespace std;
 
-// some declaretion for sqlite
-ostringstream tablecode, sqlcode;
-string query;
+// These are for the sqlite bind SECOND EXAMPLE: 
+std::ostringstream tablecode, sqlcode;
+std::string query;
+
+
 
 // call back funtion for sqlite
 int callback(void* NotUsed, int argc, char** argv, char** azColName) {
@@ -44,10 +46,42 @@ int callback(void* NotUsed, int argc, char** argv, char** azColName) {
 
 int main() {
 
+    // CREATING THE DB FILE
+    // 
+    // Pointer to SQLite connection
+    sqlite3* db;
+
+
+    // Save any error messages
+    char* zErrMsg = 0;
+
+    // Save the result 
+    int rc;
+
+    // Save any SQL
+    string sql;
+
+
+    // Save the result of opening the file. DB file has opened.
+    rc = sqlite3_open("example.db", &db);
+
+
+    // Check the Sqlite connection. This if statement is for unsuccesful connection.
+    if (rc) {
+        // Show an error message
+        cout << "DB Error: " << sqlite3_errmsg(db) << endl;
+        // Close the connection
+        sqlite3_close(db);
+        // Return an error
+        return(1);
+    }
+    // CREATING THE DB FILE END
+
+
     char c; // to stop the code
 
     ifstream file;
-    file.open("test.txt");
+    file.open("test1.txt");
 
     // START CALCULATION OF NUMBER OF ROWS AND COLUMNS
     string line = "";
@@ -56,32 +90,32 @@ int main() {
     int no_of_rows = 0;     // Number of rows
     int no_of_columns = 0; // Number of columns
 
-    
 
+    vector<std::string> column_array;
+    vector<std::string> title_array;
     // Open the file and get lines to array_str named vector
     if (file.is_open()) {
         while (getline(file, line)) {
             array_str.push_back(line);
-            
-            
         }
 
+       
         string row_single_string = array_str[1]; // Get any line from array_str vector to calcuate  # of rows and columns
+
         const char* delim = "\t"; // delimeter to calculate column number
 
-        vector<std::string> column_array;
-        vector<std::string> title_array;
+       
+      
         tokenize(row_single_string, delim, column_array);
         tokenize(array_str[0], delim, title_array);
-
+       
         no_of_rows = array_str.size(); // lines number = row number
-        no_of_columns = column_array.size(); // column number 
-    }
-
-    for (int i = 0; i < title_array.size(); i++) {
-
+        no_of_columns = column_array.size(); // column number
         
     }
+
+    
+
     // print the results 
     cout << "lines: " << no_of_rows << endl;
     cout << "columns: " << no_of_columns << endl;
@@ -90,32 +124,62 @@ int main() {
     cout << "**" << endl;
 
 
-
     // START FOR MATRIX
-    string **array_2d = new string* [no_of_rows];
+    string** array_2d = new string * [no_of_rows];
 
 
-    file.clear();
-    file.seekg(0, ios::beg);
-    for ( int i = 0; i < no_of_rows; ++i) {
+    file.clear(); 
+    file.seekg(0, ios::beg); // to prevent nonsese numbers
+
+    for (int i = 0; i < no_of_rows; i++) {
         array_2d[i] = new string[no_of_columns];
 
-        for ( int j = 0; j < no_of_columns; ++j) {
+        for (int j = 0; j < no_of_columns; j++) {
             file >> array_2d[i][j];
             cout << array_2d[i][j] << " ";
         }
         std::cout << std::endl;
     }
-    
     // END FOR MATRIX
+
+  
+    tablecode.str("");
+    //AUTOINCREMENT ensures the auto increment of primary key 
+    tablecode << "CREATE TABLE test_table1 (ID INTEGER PRIMARY KEY AUTOINCREMENT, timeindex TEXT, yvect0 TEXT, yvect1 TEXT, yvect2	TEXT, yvect3 TEXT, mvect0 TEXT, mvect1 TEXT, optqvect1 TEXT, optqr TEXT, optcost TEXT, gammacost TEXT);";
+    query = tablecode.str();
+    rc = sqlite3_exec(db, query.c_str(), callback, 0, &zErrMsg);
+
+
+
+    // These are gonna be the parameters of the prepare function to binding
+    sqlite3_stmt* res; // res is the prepared statement object
+    const char* tail;
+
+
+    sqlcode.str("");
+    sqlcode << "INSERT INTO test_table1 (timeindex, yvect0, yvect1, yvect2, yvect3,	mvect0, mvect1, optqvect1, optqr, optcost,	gammacost) VALUES(?,?,?,?,?,?,?,?,?,?,?);";
+    query = sqlcode.str();
+    rc = sqlite3_prepare_v2(db, query.c_str(), query.length(), &res, &tail);
+  
+
+
+    for (int i = 1; i < no_of_rows; i++) {
+
+        for (int j = 0; j < no_of_columns; j++) {
+
+            sqlite3_bind_text(res, j+1, array_2d[i][j].c_str(), -1, NULL);
+        }
+
+        sqlite3_step(res); // Run the SQL one or more times
+        sqlite3_clear_bindings(res); // CLEAR BINDINGS OF WRITE QUERY
+        sqlite3_reset(res); // Reset the prepared statement
+    }
+
+
+    sqlite3_finalize(res); // Destroy the object
+    
+    sqlite3_close(db);
     cin >> c; // ask user a chracter for stop
 
     return 0;
 }
-
-
-
-
-
-
-
